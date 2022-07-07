db0a6cd3940ce39e56e8d5711c44d2bf0c5fbdab
xmamalou <xmamalou@freemail.gr>
2022-07-07 19:32:14 +0300

    Update dummy file

--- Main.hs
+++ Main.hs
@@ -1,5 +1,19 @@
-import Dragonfly.Render 
+-- This is a dummy file. It's not part of the library. All library related files are 
+-- in the Dragonfly/ and c/ directories.
+
+import Dragonfly.Engine.Render 
+import Control.Concurrent
+
+window = DlfSmallWindowtype "Test" 1080 720
 
 main = do
-    x <- dlfWindowIniting "Test" 1080 720 0
+    x <- dlfWindowIniting window
+    y <- dlfVulkanIniting
+    
     print x
+    print y
+
+    threadDelay 1000000
+    
+    dlfVulkanKilling
+    dlfWindowKilling
