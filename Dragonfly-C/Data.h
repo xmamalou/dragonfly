527ecb9c16d01575cf51cf550fa96becc3b2ee6f
Christopher Mamaloukas <xmamalou@freemail.gr>
2023-05-08 21:15:20 +0300

    Did upkeep

--- Dragonfly-C/Data.h
+++ Dragonfly-C/Data.h
@@ -9,11 +9,10 @@ extern "C" {
 #define DFL_SUCCESS 0 // operation was successful
 #define DFL_FILE_NOT_FOUND 1 // Specified file was not found
 #define DFL_VULKAN_NOT_INITIALIZED 2 // Vulkan was not initialized
+#define DFL_ALLOC_ERROR 4 // Memory allocation error
+#define DFL_NO_LAYERS_FOUND 5 // No layers were found (only applicable if debug is desired but the host cannot provide tools for it)
 
-// BOOLEAN MACROS
-
-#define DFL_TRUE 1
-#define DFL_FALSE 0
+#include <stdbool.h>
 
 struct DflVec2D
 {
--- Dragonfly-C/Dragonfly.h
+++ Dragonfly-C/Dragonfly.h
@@ -1,9 +1,7 @@
 #pragma once
 
-#define DFL_VERSION_MAJOR 0
-#define DFL_VERSION_MINOR 1
-#define DFL_VERSION_PATCH 0
-
 #include "Data.h"
 #include "Render/GLFW.h"
 #include "Render/Vulkan.h"
+
+#define DFL_ENGINE_VERSION DFL_VERSION(0, 1, 0)
