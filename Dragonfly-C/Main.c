d85c1038e4bc31d377babc0faebbdb911f4131be
Christopher Mamaloukas <xmamalou@freemail.gr>
2023-05-10 02:58:44 +0300

    Used Main.

--- Dragonfly-C/Main.c
+++ Dragonfly-C/Main.c
@@ -25,40 +25,14 @@ int MAIN()
 	DflWindow	window = NULL;
 	DflSession	session = NULL;
 
-	{
-		struct DflVec2D dim = { .x = 1920, .y = 1080 };
-		struct DflVec2D view = { .x = 800, .y = 600 };
-		struct DflVec2D res = { .x = 800, .y = 600 };
+	session = dflSessionInit(NULL, DFL_SESSION_CRITERIA_NONE, DFL_GPU_CRITERIA_NONE);
+	if (session == NULL)
+		return 1;
 
-		struct DflVec2D place = { .x = 100, .y = 100 };
+	window = dflWindowCreate(NULL, &session);
+	if (window == NULL)
+		return 1;
 
-		DflWindowInfo info =
-		{
-			.dim = dim,
-			.view = view,
-			.res = res,
-			.name = "Dragonfly says: Hello World!",
-			.mode = DFL_WINDOWED,
-			.rate = 165,
-			.pos = place
-		};
-
-		struct DflSessionInfo sessionInfo =
-		{
-			.appName = "Dragonfly",
-			.appVersion = DFL_VERSION(0, 1, 0),
-			.debug = true
-		};
-
-		session = dflSessionInit(&sessionInfo, DFL_SESSION_CRITERIA_NONE, DFL_GPU_CRITERIA_NONE);
-		if (session == NULL)
-			return 1;
-
-		window = dflWindowCreate(&info);
-		if (window == NULL)
-			return 1;
-
-	}
 	Sleep(1000);
 
 	dflWindowDestroy(&window);
