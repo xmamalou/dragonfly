5e3fe25315673e96260f9664412dd1783b4d0a38
Christopher Mamaloukas <xmamalou@freemail.gr>
2023-05-10 02:56:06 +0300

    Added copyright notice

--- Dragonfly-C/Data.h
+++ Dragonfly-C/Data.h
@@ -1,4 +1,34 @@
-#pragma once
+/*
+    Copyright 2022 Christopher-Marios Mamaloukas
+
+    Redistribution and use in source and binary forms, with or without modification,
+    are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation and/or
+    other materials provided with the distribution.
+
+    3. Neither the name of the copyright holder nor the names of its contributors
+    may be used to endorse or promote products derived from this software without
+    specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+    THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+    PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+    BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+    EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef DFL_DATA_H_
+#define DFL_DATA_H_
 
 #ifdef __cplusplus
 extern "C" {
@@ -14,6 +44,7 @@ extern "C" {
 
 #include <stdbool.h>
 
+// A generic 2D vector that can refer to anything that needs 2 coordinates.
 struct DflVec2D
 {
 	int x;
@@ -25,3 +56,5 @@ struct DflVec2D
 #ifdef __cplusplus
 }
 #endif
+
+#endif // !DFL_DATA_H_
