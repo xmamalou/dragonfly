4da2501755b8f460f3d30dfbac892f7ed00724a7
Christopher Mamaloukas <xmamalou@freemail.gr>
2023-09-11 00:18:28 +0300

    Updated Main.c to reflect changes

--- Dragonfly-C/Main.c
+++ Dragonfly-C/Main.c
@@ -13,12 +13,6 @@
 	#include <shellapi.h>
 #endif
 
-#ifdef _RELEASE && _WIN32
-	#define MAIN WinMain
-#else
-	#define MAIN main
-#endif
-
 #ifdef _WIN32
 	#pragma comment (lib, "shell32.lib")
 	#pragma comment (lib, "user32.lib")
@@ -29,19 +23,21 @@
 #include "Dragonfly.h"
 #include <GLFW/glfw3.h>
 
-int MAIN()
+int main()
 {
+	int error = 0;
+
 	DflWindow	window = NULL;
 	DflSession	session = NULL;
 	DflDevice	device = NULL;
 	
-	struct DflSessionInfo info = { "Dragonfly", DFL_VERSION(0, 1, 0), DFL_SESSION_CRITERIA_DO_DEBUG};
+	struct DflSessionInfo info = { "Dragonfly", DFL_VERSION(0, 1, 0), DFL_SESSION_CRITERIA_NONE};
 	struct DflWindowInfo winfo = {
 		.dim = {1820, 1080},
 		.view = {1820, 1080},
 		.vsync = true,
 		.name = "Dragonfly", 
-		//.hIcon = dflImageReferenceFromFileGet("Resources/bugs.png"), 
+		.hIcon = dflImageReferenceFromFileGet("Resources/bugs.png"), 
 		.mode = DFL_WINDOWED, 
 		.rate = 165, 
 		.pos = {200, 200},
@@ -50,33 +46,50 @@ int MAIN()
 	};
 
 	session = dflSessionCreate(&info);
-	if (dflSessionErrorGet(session) < DFL_SUCCESS)
+	error = dflSessionErrorGet(session);
+	if (error < DFL_SUCCESS)
+	{
+		printf("Error: %x\n", error);
 		return 1;
+	}
 
 	window = dflWindowInit(&winfo, session);
-	if (dflWindowErrorGet(window) < DFL_SUCCESS)
+	error = dflWindowErrorGet(window);
+	if (error < DFL_SUCCESS)
+	{
+		printf("Error: %x\n", error);
 		return 1;
+	}
 
-	//dflImageDestroy(winfo.hIcon);
+	dflImageDestroy(winfo.hIcon);
 
 #ifdef _WIN32
 	dflWindowWin32AttributeSet(DFL_WINDOW_WIN32_BORDER_COLOUR, DFL_COLOR_GRAY, window);
 	dflWindowWin32AttributeSet(DFL_WINDOW_WIN32_TITLE_TEXT_COLOUR, DFL_COLOR_WHITE, window);
 	dflWindowWin32AttributeSet(DFL_WINDOW_WIN32_DARK_MODE, true, window);
 #endif
-	int choice = 0;
-	DflDevice* devices = dflSessionDevicesGet(&choice, session);
-	if (devices == NULL)
+	
+	dflSessionLoadDevices(session);
+	if (dflSessionDeviceCountGet(session) == NULL)
 		return 1;
 
-	device = dflDeviceInit(DFL_GPU_CRITERIA_NONE, 0, devices, session);
-	if (dflDeviceErrorGet(device) < DFL_SUCCESS)
+	dflSessionInitDevice(DFL_GPU_CRITERIA_NONE, DFL_CHOOSE_ON_RANK, session);
+	error = dflSessionErrorGet(session);
+	if (error < DFL_SUCCESS)
+	{
+		printf("Error: %x\n", error);
 		return 1;
-	free(devices);
+	}
 
-	dflWindowBindToDevice(window, device);
-	if(dflWindowErrorGet(window) < DFL_SUCCESS)
+	printf("Rank: %d", dflSessionDeviceRankGet(0, session));
+
+	dflWindowBindToDevice(window, 0, session);
+	error = dflWindowErrorGet(window);
+	if (error < DFL_SUCCESS)
+	{
+		printf("Error: %x\n", error);
 		return 1;
+	}
 
 	struct DflVec2D newDim = { 800, 600 };
 	dflWindowReshape(DFL_DIMENSIONS, newDim, window);
@@ -85,26 +98,13 @@ int MAIN()
 	dflWindowWin32AttributeSet(DFL_WINDOW_WIN32_FULL_WINDOW_AVAILABLE, true, window);
 #endif
 
-	DflMemoryPool pool = dflMemoryPoolCreate(16, true, device); // 16 ints size (hence 16 * 4 bytes = 64 bytes)
-	if (dflMemoryPoolErrorGet(pool) < DFL_SUCCESS)
-        return 1;
-
-	dflMemoryPoolExpand(16, pool, false, device); // 16 ints size (hence 16 * 4 bytes = 64 bytes) gotten from the GPU
-	if (dflMemoryPoolErrorGet(pool) < DFL_SUCCESS)
-		return 1;
-	printf("\nMEMORY POOL SIZE: %d\nMEMORY POOL BLOCK COUNT: %d\n", dflMemoryPoolSizeGet(pool), dflMemoryPoolBlockCountGet(pool));
-
 	while ((dflWindowShouldCloseGet(window) == false))
 		glfwPollEvents();
 
-	struct DflVec2D pos = { 0, 0 };
-
-	dflWindowUnbindFromDevice(window, device);
-
-	dflMemoryPoolDestroy(pool);
-
+	dflWindowUnbindFromDevice(window);
 	dflWindowTerminate(window, session);
-	dflDeviceTerminate(device, session);
+
+	dflSessionTerminateDevice(0, session);
 	dflSessionDestroy(session);
 
 	glfwTerminate();
