9feb5b3e128f0b0da72e9d1b4ca36edacb256f0a
Christopher Mamaloukas <xmamalou@freemail.gr>
2024-05-31 22:00:09 +0300

    Removed modules in favour of header file

--- Dragonfly/Dragonfly.Hardware.Session.cxx
+++ Dragonfly/Dragonfly.Hardware.Session.cxx
@@ -14,9 +14,8 @@
    limitations under the License.
 */
 
-module;
+#include "Dragonfly.hxx"
 
-#include <array>
 #include <iostream>
 #include <vector>
 #include <thread>
@@ -25,67 +24,66 @@ module;
 #include <Windows.h>
 #include <winternl.h>
 #include <powerbase.h>
-#define VK_USE_PLATFORM_WIN32_KHR
-#include <vulkan/vulkan.h>
-
-module Dragonfly.Hardware.Session;
-
-import Dragonfly.Observer;
 
 namespace DflHW = Dfl::Hardware;
-namespace DflOb = Dfl::Observer;
-
-// PROCESSOR_POWER_INFORMATION definition
-
-typedef struct _PROCESSOR_POWER_INFORMATION {
-    ULONG Number;
-    ULONG MaxMhz;
-    ULONG CurrentMhz;
-    ULONG MhzLimit;
-    ULONG MaxIdleState;
-    ULONG CurrentIdleState;
-} PROCESSOR_POWER_INFORMATION, * PPROCESSOR_POWER_INFORMATION;
 
 //
 
-static inline uint32_t INT_GetProcessorCount(){
+static inline uint32_t INT_GetProcessorCount() 
+{
     SYSTEM_INFO sysInfo;
     GetSystemInfo(&sysInfo);
     return sysInfo.dwNumberOfProcessors;
-};
+}
 
-static inline uint64_t INT_GetProcessorSpeed(const uint32_t& processorCount) {
+static inline uint64_t INT_GetProcessorSpeed(const uint32_t& processorCount) 
+{
+    typedef struct _PROCESSOR_POWER_INFORMATION {
+        ULONG Number;
+        ULONG MaxMhz;
+        ULONG CurrentMhz;
+        ULONG MhzLimit;
+        ULONG MaxIdleState;
+        ULONG CurrentIdleState;
+    } PROCESSOR_POWER_INFORMATION;
+    std::vector<PROCESSOR_POWER_INFORMATION> cpuInfos(processorCount);
     
-    std::vector<PROCESSOR_POWER_INFORMATION> cpuInfos;
-    cpuInfos.resize(processorCount);
-    
-    ULONG size{ processorCount*sizeof(PROCESSOR_POWER_INFORMATION) };
-    
-    NTSTATUS status{ CallNtPowerInformation(
-                        ProcessorInformation,
-                        nullptr,
-                        0,
-                        cpuInfos.data(),
-                        size) };
-
-    if (status != 0) {
-        return 0;
+    {
+        ULONG size{ 
+                processorCount* static_cast<ULONG>(sizeof(PROCESSOR_POWER_INFORMATION)) };
+        if (NTSTATUS status{ CallNtPowerInformation(
+                            ProcessorInformation,
+                            nullptr,
+                            0,
+                            cpuInfos.data(),
+                            size) }; 
+            status != 0) {
+            return 0;
+        }
     }
 
     return cpuInfos[0].CurrentMhz;
 }
 
-static inline DflHW::Session::Processor INT_GetProcessor() {
+static inline auto INT_GetProcessor() 
+-> DflHW::Session::Characteristics::Processor
+{
     const uint32_t processorCount{ INT_GetProcessorCount() };
 
     return { processorCount, INT_GetProcessorSpeed(processorCount) };
 }
 
-static inline uint64_t INT_GetMemory() {
+static inline uint64_t INT_GetMemory()
+{
     MEMORYSTATUSEX memInfo;
     memInfo.dwLength = sizeof(memInfo);
     GlobalMemoryStatusEx(&memInfo);
-    return ( memInfo.ullTotalPhys / (1024*1024) );
+    return ( memInfo.ullTotalPhys / Dfl::Mega );
+}
+
+static inline DflHW::Session::Characteristics INT_GetCharacteristics() 
+{
+    return { INT_GetProcessor(), INT_GetMemory() };
 }
 
 // internal for InitVulkan
@@ -94,7 +92,7 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL INT_dflDebugCLBK(
     VkDebugUtilsMessageSeverityFlagBitsEXT      severity,
     VkDebugUtilsMessageTypeFlagsEXT             type,
     const VkDebugUtilsMessengerCallbackDataEXT* data,
-    void* userData)
+    void* userData) 
 {
     if (severity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT) {
         std::cout << "\n\x1b[31mVULKAN ENCOUNTERED AN ERROR\n===ERROR===\x1b[0m\n <<" << data->pMessage << "\n";
@@ -112,9 +110,8 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL INT_dflDebugCLBK(
     return VK_FALSE;
 }
 
-static DflHW::Session::Error INT_InitDebugger(
-    const VkInstance& instance,
-    VkDebugUtilsMessengerEXT& debugger){
+static VkDebugUtilsMessengerEXT INT_InitDebugger(const VkInstance& instance)
+{
     const VkDebugUtilsMessengerCreateInfoEXT debugInfo = {
         .sType{ VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT },
         .pNext{ nullptr },
@@ -129,40 +126,46 @@ static DflHW::Session::Error INT_InitDebugger(
     const PFN_vkCreateDebugUtilsMessengerEXT createDebug{
         (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT") };
     if (createDebug == nullptr) {
-        return DflHW::Session::Error::VkDebuggerCreationError;
-    }
-    if (createDebug(
-        instance,
-        &debugInfo,
-        NULL,
-        &debugger) != VK_SUCCESS) {
-        return DflHW::Session::Error::VkDebuggerCreationError;
+        vkDestroyInstance(
+            instance,
+            nullptr);
+        throw Dfl::Error::HandleCreation(L"Unable to get required function handle for debugger");
     }
 
-    return DflHW::Session::Error::Success;
+    VkDebugUtilsMessengerEXT debugger{ nullptr };
+    if (createDebug(
+            instance,
+            &debugInfo,
+            NULL,
+            &debugger) != VK_SUCCESS) {
+        vkDestroyInstance(
+            instance,
+            nullptr);
+        throw Dfl::Error::HandleCreation(L"Unable to create debugger");
+    }
+
+    return debugger;
 }
 
-static DflHW::Session::Error INT_LoadDevices(
-    const VkInstance& instance, 
-    std::vector<VkPhysicalDevice>& devices){
+static std::vector<VkPhysicalDevice> INT_LoadDevices(const VkInstance& instance) 
+{
     uint32_t deviceCount{ 0 };
     vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
-    if (deviceCount == 0)
-        return DflHW::Session::Error::VkNoDevicesError;
-    devices.resize(deviceCount); // we now reserve space for both the physical GPUs and the ones Dragonfly understands
+    if (deviceCount == 0) 
+    {
+        throw Dfl::Error::NoData(L"Unable to find Vulkan compatible devices");
+    }
+    std::vector<VkPhysicalDevice> devices(deviceCount);
     vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
 
-    return DflHW::Session::Error::Success;
+    return devices;
 }
-
-const std::array<const char*, 1> expectedLayers{
-    "VK_LAYER_KHRONOS_validation"
- };
 
 //
 
-static DflHW::Session::Handles INT_InitSession(const DflHW::Session::Info& info){
-    const VkApplicationInfo appInfo = {
+static DflHW::Session::Handles INT_InitSession(const DflHW::Session::Info& info)
+{
+    const VkApplicationInfo appInfo{
         .sType{ VK_STRUCTURE_TYPE_APPLICATION_INFO },
         .pNext{ nullptr },
         .pApplicationName{ info.AppName.data() },
@@ -171,19 +174,24 @@ static DflHW::Session::Handles INT_InitSession(const DflHW::Session::Info& info)
         .apiVersion{ VK_API_VERSION_1_3 }
     };
 
-    std::vector<const char*> extensions{
+    constexpr std::array<const char*, 4> extensions{
         VK_KHR_SURFACE_EXTENSION_NAME,
         "VK_KHR_win32_surface",
-        VK_KHR_DISPLAY_EXTENSION_NAME
+        VK_KHR_DISPLAY_EXTENSION_NAME,
+        VK_EXT_DEBUG_UTILS_EXTENSION_NAME
+    };
+    constexpr std::array<const char*, 1> expectedLayers{
+        "VK_LAYER_KHRONOS_validation"
     };
 
-    if ( info.DoDebug == true ){
-        extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    if ( info.DoDebug == true )
+    {
         uint32_t count{ 0 };
         std::vector<VkLayerProperties> layerProperties{ };
         vkEnumerateInstanceLayerProperties(&count, nullptr);
-        if (count == 0){
-            throw DflHW::Session::Error::VkNoLayersError;
+        if (count == 0)
+        {
+            throw Dfl::Error::NoData(L"No layers found in machine");
         }
         layerProperties.resize(count);
         vkEnumerateInstanceLayerProperties(&count, layerProperties.data());
@@ -191,13 +199,17 @@ static DflHW::Session::Handles INT_InitSession(const DflHW::Session::Info& info)
         // check desired layer availability
         for (auto& layer : expectedLayers)
         {
-            for (uint32_t i{ 0 }; i < count; i++)
+            for ( auto pProperty{ layerProperties.begin() }; 
+                  pProperty != layerProperties.end(); 
+                  pProperty++ )
             {
-                if (strcmp(layerProperties[i].layerName, layer) == 0) {
+                if ( strcmp(pProperty->layerName, layer) == 0 ) 
+                {
                     break;
                 }
-                if ( i == count - 1) {
-                    throw DflHW::Session::Error::VkNoExpectedLayersError;
+                if ( pProperty == layerProperties.end() - 1 ) 
+                {
+                    throw Dfl::Error::NoData(L"The requested layers are not present");
                 }
             }
         }
@@ -209,60 +221,45 @@ static DflHW::Session::Handles INT_InitSession(const DflHW::Session::Info& info)
         .flags{ 0 },
         .pApplicationInfo{ &appInfo },
         .enabledLayerCount{ info.DoDebug == true ? static_cast<uint32_t>(expectedLayers.size()) : 0 },
-        .ppEnabledLayerNames{ info.DoDebug == true ? expectedLayers.data() : nullptr },
-        .enabledExtensionCount{ static_cast<uint32_t>(extensions.size()) },
+        .ppEnabledLayerNames{ expectedLayers.data() },
+        .enabledExtensionCount{ static_cast<uint32_t>(extensions.size()) - (info.DoDebug == true ? 0 : 1) },
         .ppEnabledExtensionNames{ extensions.data() },
     };
 
     VkInstance instance{ nullptr };
     switch (const VkResult vkError{ vkCreateInstance(
-                                   &instInfo,
-                                   nullptr,
-                                   &instance) }) {
+                                       &instInfo,
+                                       nullptr,
+                                       &instance) }) 
+    {
     case VK_SUCCESS:
         break;
     case VK_ERROR_INCOMPATIBLE_DRIVER:
-        throw DflHW::Session::Error::VkBadDriverError;
+        throw Dfl::Error::HandleCreation(L"The Vulkan driver is incompatible");
     default:
-        throw DflHW::Session::Error::VkInstanceCreationError;
+        throw Dfl::Error::HandleCreation(L"Unable to create Vulkan instance");
     }
 
-    VkDebugUtilsMessengerEXT dbMessenger{ nullptr };
-    DflHW::Session::Error err{ DflHW::Session::Error::Success };
-    if (info.DoDebug == true) {
-        err = INT_InitDebugger(instance, dbMessenger);
-    }
-
-    if (err != DflHW::Session::Error::Success) {
-        vkDestroyInstance(instance, nullptr);
-        throw err;
-    }
-
-    std::vector<VkPhysicalDevice> devices{};
-    err = INT_LoadDevices(instance, devices);
-    if (err != DflHW::Session::Error::Success) {
-        vkDestroyInstance(instance, nullptr);
-        throw err;
-    }
-
-    return { instance, dbMessenger, devices };
+    return { instance, 
+             info.DoDebug ? INT_InitDebugger(instance) : nullptr, 
+             INT_LoadDevices(instance) };
 }
 
 DflHW::Session::Session(const Info& info) 
-try : GeneralInfo(new Info(info)), 
-      CPU( INT_GetProcessor() ),
-      Memory( INT_GetMemory() ),
-      Instance( INT_InitSession(info)) { }
-catch (Error e) { this->ErrorCode = e; }
+: pInfo( new Info(info) ), 
+  pCharacteristics( new Characteristics(INT_GetCharacteristics()) ),
+  Instance( INT_InitSession(info) ) { }
 
 DflHW::Session::~Session()
 {
-    if (this->Instance.hDbMessenger != nullptr){ // that means debugging was enabled.
-        const PFN_vkDestroyDebugUtilsMessengerEXT destroyDebug{ 
-            (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(
-                                                    this->Instance, 
-                                                    "vkDestroyDebugUtilsMessengerEXT") };
-        if ( destroyDebug != nullptr ){
+    if (this->Instance.hDbMessenger != nullptr) // that means debugging was enabled.
+    { 
+        if ( const PFN_vkDestroyDebugUtilsMessengerEXT destroyDebug{
+                (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(
+                                                        this->Instance,
+                                                        "vkDestroyDebugUtilsMessengerEXT") }; 
+             destroyDebug != nullptr )
+        {
             destroyDebug(
                 this->Instance, 
                 this->Instance.hDbMessenger, 
@@ -270,13 +267,12 @@ DflHW::Session::~Session()
         }
     }
 
-    if( this->Instance.hInstance != nullptr ){
-        vkDestroyInstance(this->Instance, nullptr);
-    }
+    vkDestroyInstance(this->Instance, nullptr);
 }
 
 inline const std::string DflHW::Session::GetDeviceName(const uint32_t& index) const noexcept {
-    if(index >= this->Instance.hDevices.size()) {
+    if(index >= this->Instance.hDevices.size()) 
+    {
         return "N/A";
     }
     
